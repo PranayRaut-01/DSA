749a077f497efd4852cf863373dae3b944f67715
Pranay Raut <pranay@Pranays-MacBook-Air.local>
2023-11-26 23:46:28 +0530

    completed problem

--- dataStructures/Arrays/getSingleElement.js
+++ dataStructures/Arrays/getSingleElement.js
@@ -1,10 +1,15 @@
 // Find the number that appears once, and the other numbers twice
 // Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
 
+
+
+//optimal approach xor property
 function getSingleEl(arr) {
-    const queue =[]
-    
-    
+    let xorr = 0
+    for(let i=0;i<arr.length;i++){
+        xorr = xorr ^ arr[i]    // xor property 1^1 = 0 , 0^1 =1
+    }
+    return xorr   
 }
 
 console.log(getSingleEl([1,1,2,3,3,4,4]))
